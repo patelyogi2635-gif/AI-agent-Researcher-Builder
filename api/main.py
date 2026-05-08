90d9f08f0f3c62d9149c6d986f9b01cf52e7e035
Patel yogi <patelyogi2635@gmail.com>
2026-05-08 10:36:15 +0530

    fix cors trailing slash

--- api/main.py
+++ api/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://synapse-ai-delta-coral.vercel.app/",   # ← your vercel URL
+        "https://synapse-ai-delta-coral.vercel.app",   # ← your vercel URL
         "https://*.vercel.app",
     ],
     allow_methods=["*"],
